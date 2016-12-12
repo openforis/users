194496f6eb5e921ba22ab7aa64baa5031ac5a394
fefrain <fefrain@users.noreply.github.com>
2016-12-12 15:12:20 +0100

    Add user

--- src/main/java/org/openforis/users/web/Server.java
+++ src/main/java/org/openforis/users/web/Server.java
@@ -1,9 +1,11 @@
 package org.openforis.users.web;
 
 import static spark.Spark.get;
+import static spark.Spark.post;
 import static spark.Spark.staticFileLocation;
 
 import org.openforis.users.manager.EntityManagerFactory;
+import org.openforis.users.model.User;
 
 import spark.Request;
 import spark.Response;
@@ -27,9 +29,9 @@ public class Server implements SparkApplication {
 
 		get("/hello/:name", Views.home);
 		
-		get("/users/all", JSON_CONTENT_TYPE, listAllUsers, new JsonTransformer());
+		get("/users/all", listAllUsers, new JsonTransformer());
 		
-		
+		post("/users/", addUser, new JsonTransformer());
 
 	}
 	
@@ -37,4 +39,13 @@ public class Server implements SparkApplication {
 		return EntityManagerFactory.getUserManager().listAll();
 	};
 
+	private Route addUser = (Request req, Response rsp) -> {
+		String username = req.queryParams("username");
+		String password = req.queryParams("password");
+		User user = new User();
+		user.setUsername(username);
+		user.setPassword(password);
+		EntityManagerFactory.getUserManager().save(user);
+		return user;
+	};
 }
