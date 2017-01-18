70631b1390c127779ed880c248b8f3c4d3acb724
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2017-01-18 17:31:02 +0100

    some changes on the user endpoints, cors enabled

--- src/main/java/org/openforis/users/web/Server.java
+++ src/main/java/org/openforis/users/web/Server.java
@@ -1,9 +1,10 @@
 package org.openforis.users.web;
 
-import static spark.Spark.delete;
+import static spark.Spark.staticFileLocation;
 import static spark.Spark.get;
 import static spark.Spark.post;
-import static spark.Spark.staticFileLocation;
+import static spark.Spark.patch;
+import static spark.Spark.delete;
 
 import java.io.File;
 import java.net.URL;
@@ -20,6 +21,7 @@ import freemarker.template.TemplateExceptionHandler;
 import spark.Request;
 import spark.Response;
 import spark.Route;
+import spark.Spark;
 import spark.servlet.SparkApplication;
 import spark.template.freemarker.FreeMarkerEngine;
 
@@ -32,7 +34,7 @@ import spark.template.freemarker.FreeMarkerEngine;
 public class Server implements SparkApplication {
 
 	private static final String JSON_CONTENT_TYPE = "application/json";
-	
+
 	private static final UserManager USER_MANAGER = EntityManagerFactory.getInstance().getUserManager();
 
 	private JsonTransformer jsonTransformer;
@@ -44,12 +46,32 @@ public class Server implements SparkApplication {
 		cfg.setDefaultEncoding("UTF-8");
 		cfg.setTemplateExceptionHandler(TemplateExceptionHandler.HTML_DEBUG_HANDLER);
 		// cfg.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
-		//cfg.setLogTemplateExceptions(false);
+		// cfg.setLogTemplateExceptions(false);
 		return new FreeMarkerEngine(cfg);
+	}
+
+	private static void enebleCORS() {
+		Spark.options("/*", (request, response) -> {
+			String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+			if (accessControlRequestHeaders != null) {
+				response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+			}
+			String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+			if (accessControlRequestMethod != null) {
+				response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+			}
+			return "OK";
+		});
+		Spark.before((request, response) -> {
+			response.header("Access-Control-Allow-Origin", "*");
+		});
 	}
 
 	@Override
 	public void init() {
+
+		Server.enebleCORS();
+
 		jsonTransformer = new JsonTransformer();
 
 		FreeMarkerEngine renderer = null;
@@ -63,18 +85,17 @@ public class Server implements SparkApplication {
 		staticFileLocation("/public");
 
 		get("/hello/:name", Views.home);
-
 		get("/hello2/:name", Views.home2, renderer);
-		
-		get("/users", findUsersByParameters, new JsonTransformer());
 
-		post("/users", JSON_CONTENT_TYPE, addUser, new JsonTransformer());
-		
-		delete("/users/:id", deleteUser, new JsonTransformer());
+		// USER
+		get("/user", findUsers, new JsonTransformer());
+		post("/user", JSON_CONTENT_TYPE, addUser, new JsonTransformer());
+		patch("/user", JSON_CONTENT_TYPE, editUser, new JsonTransformer());
+		delete("/user/:id", JSON_CONTENT_TYPE, deleteUser, new JsonTransformer());
 
 	}
-	
-	private Route findUsersByParameters = (Request req, Response rsp) -> {
+
+	private Route findUsers = (Request req, Response rsp) -> {
 		String username = req.queryParams("username");
 		if (username == null) {
 			return USER_MANAGER.findAll();
@@ -93,19 +114,36 @@ public class Server implements SparkApplication {
 		Map<String, Object> bodyMap = jsonTransformer.parse(body);
 		String username = bodyMap.get("username").toString();
 		String password = bodyMap.get("password").toString();
-
+		//
 		User user = new User();
 		user.setUsername(username);
 		user.setPassword(password);
-		
+		//
 		USER_MANAGER.save(user);
 		return user;
 	};
-	
+
+	private Route editUser = (Request req, Response rsp) -> {
+		String body = req.body();
+		Map<String, Object> bodyMap = jsonTransformer.parse(body);
+		//
+		Long id = Long.parseLong(bodyMap.get("id").toString());
+		String username = bodyMap.get("username").toString();
+		String password = bodyMap.get("password").toString();
+		//
+		User user = USER_MANAGER.findById(id);
+		user.setUsername(username);
+		user.setPassword(password);
+		//
+		USER_MANAGER.save(user);
+		return user;
+	};
+
 	private Route deleteUser = (Request req, Response rsp) -> {
-		String idStr = req.params("id");
-		long id = Long.parseLong(idStr);
+		String idParam = req.params("id");
+		Long id = Long.parseLong(idParam);
+		//
 		return USER_MANAGER.deleteById(id);
 	};
-		
+
 }
