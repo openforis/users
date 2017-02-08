260d64286be38a4fdb03361f8a7ece6f2141c4c9
rfontanarosa <robertofontanarosa@gmail.com>
2017-02-08 15:45:50 +0100

    added a matcher to bypass security for OPTIONS' http requests

--- src/main/java/org/openforis/users/auth/UsersConfigFactory.java
+++ src/main/java/org/openforis/users/auth/UsersConfigFactory.java
@@ -3,6 +3,8 @@ package org.openforis.users.auth;
 import org.pac4j.core.client.Clients;
 import org.pac4j.core.config.Config;
 import org.pac4j.core.config.ConfigFactory;
+import org.pac4j.core.context.HttpConstants.HTTP_METHOD;
+import org.pac4j.core.matching.HttpMethodMatcher;
 import org.pac4j.http.client.direct.DirectBasicAuthClient;
 
 public class UsersConfigFactory implements ConfigFactory {
@@ -11,9 +13,9 @@ public class UsersConfigFactory implements ConfigFactory {
 		final DirectBasicAuthClient directBasicAuthClient = new DirectBasicAuthClient();
 		directBasicAuthClient.setAuthenticator(new UsersAuthenticator());
 		directBasicAuthClient.setProfileCreator(new UsersProfileCreator());
-		//Clients clients = new Clients(directBasicAuthClient);
-		//Config config = new Config(clients);
-		Config config = new Config(directBasicAuthClient);
+		Clients clients = new Clients(directBasicAuthClient);
+		Config config = new Config(clients);
+		config.setMatcher(new HttpMethodMatcher(HTTP_METHOD.GET, HTTP_METHOD.POST, HTTP_METHOD.PUT, HTTP_METHOD.DELETE));
 		config.setHttpActionAdapter(new UsersHttpActionAdapter());
 		return config;
 	}
