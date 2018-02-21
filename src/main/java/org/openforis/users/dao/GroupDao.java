7581bcadaaf89d7b5597f305c5fa3908360138a0
rfontanarosa <robertofontanarosa@gmail.com>
2018-02-21 16:48:22 +0100

    the group endpoint now doesn't return the group logos

--- src/main/java/org/openforis/users/dao/GroupDao.java
+++ src/main/java/org/openforis/users/dao/GroupDao.java
@@ -3,22 +3,24 @@ package org.openforis.users.dao;
 import static org.openforis.users.jooq.tables.OfGroup.OF_GROUP;
 import static org.openforis.users.jooq.tables.OfUserGroup.OF_USER_GROUP;
 
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.jooq.Condition;
 import org.jooq.Configuration;
 import org.jooq.DSLContext;
-import org.jooq.SelectWhereStep;
+import org.jooq.Record7;
+import org.jooq.SelectJoinStep;
 import org.jooq.impl.DSL;
 import org.openforis.users.jooq.tables.daos.OfGroupDao;
-import org.openforis.users.jooq.tables.records.OfGroupRecord;
 import org.openforis.users.manager.GroupManager.SearchParameters;
 import org.openforis.users.model.Group;
 
 /**
  * 
  * @author S. Ricci
+ * @author R. Fontanarosa
  *
  */
 public class GroupDao extends OfGroupDao {
@@ -28,7 +30,11 @@ public class GroupDao extends OfGroupDao {
 	}
 
 	public List<Group> loadAll(SearchParameters params) {
-		SelectWhereStep<OfGroupRecord> query = dsl().selectFrom(OF_GROUP);
+		SelectJoinStep<Record7<String, String, String, Boolean, Boolean, String, Timestamp>> query = dsl()
+				.select(OF_GROUP.NAME, OF_GROUP.LABEL, OF_GROUP.DESCRIPTION,
+						OF_GROUP.ENABLED, OF_GROUP.SYSTEM_DEFINED,
+						OF_GROUP.VISIBILITY_CODE, OF_GROUP.CREATION_DATE)
+				.from(OF_GROUP);
 		if (params != null) {
 			List<Condition> conditions = new ArrayList<>();
 			if (params.getName() != null) {
